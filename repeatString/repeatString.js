a472b26104a39287f7014d65f04fc2b3b30b8983
James Lafferty <james@j-lafferty.net>
2018-08-05 16:25:57 -0700

    add function to repeat given string for given number of times

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-var repeatString = function() {
-
-}
+var repeatString = function(text, n) {
+    var result = "";
+    if (n >= 0) {
+        for (i = 0; i < n; i++) {
+            result += text;
+        };
+        return result;
+    } else {
+        return "ERROR"
+    };
+};
 
 module.exports = repeatString
