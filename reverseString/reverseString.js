ad9e8668e63935574ecabe81e71eae03c88c4d1e
James Lafferty <james@j-lafferty.net>
2018-08-05 16:55:10 -0700

    add function to reverse given string

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-var reverseString = function() {
+var reverseString = function(text) {
+    var reverseStr = "";
 
-}
+    for (i = 0; i <= text.length; i++) {
+        reverseStr += text.charAt(text.length - i);
+    };
+    return reverseStr;
+};
 
 module.exports = reverseString
