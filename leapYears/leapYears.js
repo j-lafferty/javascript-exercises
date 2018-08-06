a2b79fe53f9579bbaa5fbf6b302b23be48246906
James Lafferty <james@j-lafferty.net>
2018-08-05 18:10:04 -0700

    add function to verify if a given year is a leap year

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,11 @@
-var leapYears = function() {
-
+var leapYears = function(year) {
+    var leapYear = false;
+    if (year % 4 === 0) {
+        if (!(year % 100 === 0 && year % 400 != 0)) {
+            leapYear = true;
+        };
+    };
+    return leapYear;
 }
 
 module.exports = leapYears
