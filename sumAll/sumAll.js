cc4f6e108a8a8bb726fa8a78d9ed9d1ea977d563
James Lafferty <james@j-lafferty.net>
2018-08-05 17:42:59 -0700

    add function to sum all given numbers and return 'ERROR' for given non-number values

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-var sumAll = function() {
-
-}
+var sumAll = function(a, b) {
+    var sum = 0;
+    if ((a >= 0 && b >= 0 && typeof a === "number" && typeof b === "number")) {
+        if (a < b) {
+            for (i = a; i <= b; i++) {
+                sum += i;
+            };
+            return sum;
+        } else {
+            for (i = a; i >= b; i--) {
+                sum += i;
+            };
+            return sum;
+        };
+    } else {
+        return 'ERROR';
+    };
+    
+};
 
 module.exports = sumAll
