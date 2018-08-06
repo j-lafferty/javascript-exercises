bbd08ef8c7c528e563497104bf0d44364378f273
James Lafferty <james@j-lafferty.net>
2018-08-05 18:22:16 -0700

    add functions that convert temperature scales

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,10 @@
-var ftoc = function() {
-  
-}
+var ftoc = function(fDegree) {
+  return Math.round(((fDegree - 32) * 0.5556) * 10) / 10;
+};
 
-var ctof = function() {
-  
-}
+var ctof = function(cDegree) {
+  return Math.round(((cDegree * 1.8) + 32) * 10) / 10;
+};
 
 module.exports = {
   ftoc,
