13ef70c720f148bfbd0f5df3892c7f0bc99557b4
James Lafferty <james@j-lafferty.net>
2018-09-03 16:40:18 -0700

    Inital commit

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-var palindromes = function() {
+var palindromes = function(word) {
+    var word = word;
+    var reverse = '';
 
-}
+    for (i = word.length; i > 0; i--) {
+        reverse += word.charAt(i - 1);
+    };
+    if (word === reverse) {
+        return true;
+    };
+};
 
 module.exports = palindromes
