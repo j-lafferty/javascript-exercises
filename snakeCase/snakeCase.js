c202c6b19aa4d9fc2c6e68d7f03340ac4b5ee7b3
James Lafferty <james@j-lafferty.net>
2018-09-03 18:54:37 -0700

    Add regex for kebab case and camel case

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,6 @@
 var snakeCase = function(str) {
-    var strSnake = str.replace(/[ -]+/g, '_').replace(/[^a-zA-z\_]+/g, '').toLowerCase();
+    var strSnake = str.replace(/[ -\.]+/g, '_').replace(/[^a-zA-z\_]+/g, '').replace(/\.?([A-Z]+)/g, function (y){return "_" + y.toLowerCase()}).replace(/^_/, "").replace(/\_(?=_)+/g, '').replace(/\_$/, '').toLowerCase();
+
 
     return strSnake;
 }
