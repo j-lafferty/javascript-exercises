3b71e498f272d84e7da61b16a1b99b95b3f6d586
James Lafferty <james@j-lafferty.net>
2018-09-03 17:18:39 -0700

    Change var to 'str' and 'strReverse', add 'return false'

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,12 +1,14 @@
-var palindromes = function(word) {
-    var word = word.toLowerCase().replace(/[^A-Za-z]+/g, '');
-    var reverse = word.split('').reverse().join('');
+var palindromes = function(str) {
+    var str = str.toLowerCase().replace(/[^A-Za-z]+/g, '');
+    var strReverse = str.split('').reverse().join('');
 
     /* for (i = word.length; i > 0; i--) {
         reverse += word.charAt(i - 1);
     }; */
-    if (word === reverse) {
+    if (str === strReverse) {
         return true;
+    } else {
+        return false;
     };
 };
 
