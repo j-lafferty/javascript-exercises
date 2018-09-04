0f58779a9d8fc67f2ae593c1ad79883f0b9d4b74
James Lafferty <james@j-lafferty.net>
2018-09-03 18:29:40 -0700

    Initial commit

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,7 @@
-var snakeCase = function() {
+var snakeCase = function(str) {
+    var strSnake = str.replace(/[ -]+/g, '_').replace(/[^a-zA-z\_]+/g, '').toLowerCase();
 
+    return strSnake;
 }
 
 module.exports = snakeCase
