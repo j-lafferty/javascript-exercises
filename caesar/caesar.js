419a74c1f4c8376f11e87398e3455df66e83b4d0
James Lafferty <james@j-lafferty.net>
2018-09-08 14:01:41 -0700

    Add if-statement to wrap negative shift amount

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,4 +1,11 @@
 var caesar = function(str, shift) {
+
+    //wrap shift if negative
+    if (shift < 0) {
+        return caesar (str, shift + 26);
+    };
+
+    //encoded output
     var encodeStr = "";
 
     /* 
@@ -6,14 +13,17 @@ var caesar = function(str, shift) {
     upper A thru Z = 65 thru 90.
     lower a thru z = 97 thru 122.
     */
+
     for (var i = 0; i < str.length; i++) {
         var strChar = str.charCodeAt(i);
-        if (strChar >= 65 && strChar <= 90) {
-            encodeStr += String.fromCharCode((strChar - 65 + shift) % 26 + 65);
-        } else if (strChar >= 97 && strChar <= 122) {
-            encodeStr += String.fromCharCode((strChar - 97 + shift) % 26 + 97);
+        var sum = strChar + shift;
+
+        if (strChar >= 65 && strChar <= 90) {           //check for upper A thru Z.
+            encodeStr += String.fromCharCode((sum - 65) % 26 + 65);
+        } else if (strChar >= 97 && strChar <= 122) {   //check for lower a thru z.
+            encodeStr += String.fromCharCode((sum - 97) % 26 + 97);
         } else {
-            encodeStr += String.fromCharCode(strChar);
+            encodeStr += String.fromCharCode(strChar);  //keep punctuation.
         };
     };
     return encodeStr;
