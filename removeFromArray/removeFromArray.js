d7d9861369455c7d5411d0f919e2fd7745e40381
James Lafferty <james@j-lafferty.net>
2018-09-09 21:24:53 -0700

    Initial commit

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
 var removeFromArray = function() {
+    var arg;
+    var mainArr = [];
+    var arguArr = [];
+    var check;
+    var remove;
 
-}
+    for (var i = 0; i < arguments.length; i++) {
+        arg = arguments[i];
+        if (Array.isArray(arg)) {
+            mainArr = arg.slice();
+        } else {
+            arguArr.push(arg);
+        };
+    };
+
+    for (var i = 0; i < arguArr.length; i++) {
+        check = arguArr[i];
+        if (mainArr.indexOf(check) > 0) {
+            remove = mainArr.indexOf(check);
+            mainArr.splice(remove, 1);
+        };
+    };
+
+    return mainArr;
+};
 
 module.exports = removeFromArray
